7409c3dd08fbc848851e670aae7d37ce960e9fae
fqueta <ger.maisaqui1@gmail.com>
2025-03-07 05:55:54 -0300

    07/03/2025 02

--- resources/views/crm/painel/assinaturas.blade.php
+++ resources/views/crm/painel/assinaturas.blade.php
@@ -1,4 +1,5 @@
 {{-- <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous"> --}}
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
 @php
 // dd($arr_processo);
 $assinantes = isset($arr_processo['signers']) ? $arr_processo['signers'] : false;
@@ -8,16 +9,17 @@ $assinantes = isset($arr_processo['signers']) ? $arr_processo['signers'] : false
         font-size: 16px;
         font-weight: bold;
     }
-    .card{
-        font-size: 13px;
+    .imp-link{
+        height: 56px !important;
     }
+
 </style>
 @if (is_array($assinantes))
-    <div class="card card-secondary card-outline">
-        <div class="card-header">
+    <div class="panel panel-default panel-outline">
+        <div class="panel-header">
             {{__('Gerenciamento de assinaturas')}}
         </div>
-        <div class="card-body">
+        <div class="panel-body">
             <div class="row">
                 @foreach ($assinantes as $k=>$v )
                 {{-- {{dd($v)}} --}}
@@ -31,8 +33,8 @@ $assinantes = isset($arr_processo['signers']) ? $arr_processo['signers'] : false
                             $status_sign = 'Aguardando Assinatura';
                         }
                     @endphp
-                    <div class="card w-100">
-                        <div class="card-body">
+                    <div class="panel w-100">
+                        <div class="panel-body">
                             <div class="col-md-12">
                                 <div class="row">
                                     <div class="col-md-6">
@@ -47,7 +49,21 @@ $assinantes = isset($arr_processo['signers']) ? $arr_processo['signers'] : false
                                 </div>
                             </div>
                             <div class="col-md-12">
-                                <div class="input-group">
+                                <div class="input-group input-group-lg">
+                                    <span class="input-group-addon">{{__('Link de assinatura:')}}</span>
+                                    <input type="text" class="form-control imp-link" disabled value="{{$v['sign_url']}}" aria-label="Amount (to the nearest dollar)">
+                                    <span class="input-group-addon">
+                                        <button class="btn btn-outline-secondary dropdown-toggle" type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Ação</button>
+                                        <div class="dropdown-menu pull-right">
+                                            <a class="dropdown-item" href="javascript:void(0)" onclick="copyTextToClipboard('{{$v['sign_url']}}')">Copiar</a>
+                                            <a class="dropdown-item" target="_blank" href="{{$v['sign_url']}}">Acessar</a>
+                                            {{-- <a class="dropdown-item" href="#">Something else here</a>
+                                            <div role="separator" class="dropdown-divider"></div>
+                                            <a class="dropdown-item" href="#">Separated link</a> --}}
+                                        </div>
+                                    </span>
+                                </div>
+                                {{-- <div class="input-group">
                                     <div class="input-group-prepend">
                                         <span class="input-group-text" id="basic-addon1">{{__('Link de assinatura:')}}</span>
                                       </div>
@@ -57,19 +73,16 @@ $assinantes = isset($arr_processo['signers']) ? $arr_processo['signers'] : false
                                     <div class="dropdown-menu pull-right">
                                         <a class="dropdown-item" href="javascript:void(0)" onclick="copyTextToClipboard('{{$v['sign_url']}}')">Copiar</a>
                                         <a class="dropdown-item" target="_blank" href="{{$v['sign_url']}}">Acessar</a>
-                                        {{-- <a class="dropdown-item" href="#">Something else here</a>
-                                        <div role="separator" class="dropdown-divider"></div>
-                                        <a class="dropdown-item" href="#">Separated link</a> --}}
                                     </div>
                                     </div>
-                                </div>
+                                </div> --}}
                             </div>
                         </div>
                     </div>
                 @endforeach
             </div>
         </div>
-        {{-- <div class="card-footer text-muted">
+        {{-- <div class="panel-footer text-muted">
             Footer
         </div> --}}
     </div>
