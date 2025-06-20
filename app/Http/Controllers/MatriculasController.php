e13051a1f1bf8ab77d01339e301ab29ff6262448
fqueta <ger.maisaqui1@gmail.com>
2025-06-20 19:15:24 -0300

    20/06/2025

--- app/Http/Controllers/MatriculasController.php
+++ app/Http/Controllers/MatriculasController.php
@@ -462,7 +462,7 @@ class MatriculasController extends Controller
 							$espacoTable = false;
 						}
 						if(isset($dados['modulos']) && !empty($dados['modulos'])){
-                            $valor_matricula = isset($dados['matricula']) ? $dados['matricula'] : null;
+                            $valor_matricula = isset($dados['matricula']) ? $dados['matricula'] : @$dados['inscricao_curso'];
                             if(is_array($dados['modulos'])){
 								$arr_modu = $dados['modulos'];
 							}else{
@@ -1380,8 +1380,8 @@ class MatriculasController extends Controller
 						// $tr = str_replace('{valor}',number_format($dados['valor_curso'],2,',','.'),$tr);
 						$tr = str_replace('{valor}',number_format($ret['totalCurso'],2,',','.'),$tr);
 						$i++;
-                        $valor_matricula = isset($dados['matricula']) ? $dados['matricula'] : null;
 						// if(isset($dados['inscricao_curso'])&&$dados['inscricao_curso']>0 && $valor_matricula){
+                        $valor_matricula = isset($dados['matricula']) ? $dados['matricula'] : @$dados['inscricao_curso'];
 						if($valor_matricula){
 							$tr .= str_replace('{num}',$i,$tema2);
 							$tr = str_replace('{descricao}','Matrícula '.$dados['titulo_curso'],$tr);
