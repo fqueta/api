def38694bcb4b5e749c7dc68a01a6dba32277f13
fqueta <ger.maisaqui1@gmail.com>
2025-09-11 12:56:00 -0300

    11/09/2025

--- app/Http/Controllers/TesteController.php
+++ app/Http/Controllers/TesteController.php
@@ -36,7 +36,7 @@ class TesteController extends Controller
         // dd($ret);
         // $ret = (new MatriculasController)->grava_contrato_statico_periodo($token,$periodo='68758c8e76cfb');
         // $ret = (new MatriculasController)->grava_contrato_statico($token);
-        // $ret = (new MatriculasController)->send_to_zapSing($token,false);
+        $ret = (new MatriculasController)->send_to_zapSing($token,false);
         // $ret = (new MatriculasController)->enviar_contratos_anexos(false,$token,false);
         // $ret = (new ZapsingController )->painel_assinaturas($token);
         // $ret = (new OrcamentoController)->resumo_proposta_periodos($token);
@@ -169,11 +169,11 @@ class TesteController extends Controller
         // $ret = (new ZapsingController)->enviar_anexo($token_envelope,$url_pdf,$nome_arquivo='Termo concordancia');
         // $ret = (new MatriculasController)->link_contratos_anexos($token);
         //*teste de documentos gerados para a assinatura
-        $dm = (new MatriculasController)->dm($token);
-        $id = isset($dm['id']) ? $dm['id'] : 0;
-        // $ret = (new MatriculasController)->enviar_contratos_anexos(false,false,$dm);
-        // dd($token);
-        $ret = (new MatriculasController)->contatos_estaticos_pdf($id,true,$dm);
+        // $dm = (new MatriculasController)->dm($token);
+        // $id = isset($dm['id']) ? $dm['id'] : 0;
+        // // $ret = (new MatriculasController)->enviar_contratos_anexos(false,false,$dm);
+        // // dd($token);
+        // $ret = (new MatriculasController)->contatos_estaticos_pdf($id,true,$dm);
         // $signed_file = 'https://zapsign.s3.amazonaws.com/sandbox/dev/2025/2/pdf/f787046f-616b-4d0d-9924-769862f0d13f/63d6a30f-b5ac-4c00-ac5d-f230c95ea0ff.pdf?AWSAccessKeyId=AKIASUFZJ7JCTI2ZRGWX&Signature=RGvYrlWIkuwzB1YwQ49RLNkGhM0%3D&Expires=1741026569';
         // $ret = (new MatriculasController)->baixar_arquivo($token, $signed_file);
         // dd(count($ret));
